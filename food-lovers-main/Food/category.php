78078586f11454bec919196924c4b2addeb11711
saji37 <sajiththomas162631@gmail.com>
2022-09-03 22:06:05 +0530

    Update category.php

--- food-lovers-main/Food/category.php
+++ food-lovers-main/Food/category.php
@@ -17,8 +17,7 @@
         <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
             <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                 <h1 class="mb-8 text-3xl text-center font-medium text-blue-600">Category</h1>
-              
-                <div class="dropdown">
+               <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     Dropdown button
   </button>
@@ -30,6 +29,7 @@
 </div>
 
 
+               
 
 
               <!--  <input 
