fac2e40b9322a039ef81842ba2df4652c74e43dd
Geethanjali <geethanjaliks001@gmail.com>
2022-09-19 07:37:29 +0530

    table

---
+++ food-lovers-main/Food/table.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- ... -->
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <link href="../styles/main.css" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+table, th, td {
+  border: 1px solid white;
+  border-collapse: collapse;
+}
+th, td {
+  background-color: #96D4D4;
+}
+
+t{
+    padding-top: 100px;
+}
+</style>
+</head>
+<body>
+
+<table align='center' cellspacing=2 cellpadding=5  style="width:50%">
+<tr>
+<th>UserName</th>
+<th>Email</th>
+<th>Contact</th>
+</tr>
+<tr>
+    <td>Geethanjali</td>
+    <td>geeethanjali@gmail.com</td>
+    <td>7996201371</td>
+</tr>
+
+</body>
